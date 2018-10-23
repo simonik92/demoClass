21ebd4c591beb585560f03f5107b3a942c09c230
simonacaltea <caltea.simona@yahoo.com>
2018-10-23 15:00:39 +0300

    Initial commit

---
+++ src/webExplorations/Main.java
@@ -0,0 +1,5 @@
+package webExplorations;
+
+public class Main {
+
+}
---
+++ src/webExplorations/MediaAritmetica.java
@@ -0,0 +1,13 @@
+package webExplorations;
+
+
+public class MediaAritmetica
+{
+    public static void main (String[] args)
+    {
+        int[]myArr = {3,5};
+        int sum = myArr[0] + myArr[1];
+        int media = sum/2;
+        System.out.println("Media aritmetica a numerelor " + myArr[0] + " si " + myArr[1] + " este: " + media);
+    }
+}
